1d6331b508471a6a3a3025086499bed8ffbd50ab
NompumeleloMbhense <nsmbhense6@gmail.com>
2025-12-04 03:08:58 +0200

    chore: replace default ASP.NET landing content with custom project intro

--- obj/Debug/net8.0/ResourceBookingSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/ResourceBookingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResourceBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9d803d05943c1ef69df0951be12f9684c2332b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac92df404a8b05cbbba14183279fbe9b83023e39")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResourceBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResourceBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
